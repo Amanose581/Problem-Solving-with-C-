4c4857d3493239fa55fdd9ae0afcb42759dab70b
Amanose581 <kadrian.akabogu@gmail.com>
2024-08-17 14:34:56 +0100

    Create Self Test Exercise 1.cpp

---
+++ Chapter 1/Self Test Exercise 1.cpp
@@ -0,0 +1,23 @@
+// Kachikwulu Akabogu
+// Program that finds the sum of any two integers it is given
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Declaration of variables
+	int n1, n2, sum;
+	
+	// Prompts the user to input the two integers
+	cout << "Please enter two integers: " << endl;
+	cin >> n1 >> n2 >> endl;
+	
+	// Assigns the sum of the two integers to the variable, "sum".
+	sum = n1 + n2;
+	
+	// Outputs the value of the variable "sum".S
+	cout << sum;
+	
+	return 0;
+}
